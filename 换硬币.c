680ddb79b941e48806a6ddaf424fc65a66f0c86c
kobe1916 <47083794+kobe1916@users.noreply.github.com>
2019-07-16 22:27:11 +0800

    Update 换硬币.c

--- 换硬币.c
+++ 换硬币.c
@@ -43,3 +43,36 @@ int main()
 	system("pause");
 	return 0;
 }
+
+//3.   O(n^2)
+int main()
+{
+	int i, j, k;
+	//因为一元最多换20枚五分硬币
+	for (k = 0; k <= 20; k++)
+	{
+		for (j = 0; j <= 50; j++)
+		{
+			i = 50 - j-k;
+			if (i + 2 * j + 5 * k == 100)
+				printf("%3d %3d %3d \n", i, j, k);
+		}
+	}
+	system("pause");
+	return 0;
+}
+
+
+//4.   O(1)
+int main()
+{
+	int i, j, k;
+	for (k = 0; k < 13; k++)
+	{
+		j = 50 - 4 * k;
+		i = 50 - j - k;
+		printf("%3d %3d %3d \n", i, j, k);
+	}
+	system("pause");
+	return 0;
+}
